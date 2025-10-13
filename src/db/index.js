cc4ba13e4a5782a695f683dfabc5e29d1cc5ef9c
Arpon Dutta <arpongen3094@gmail.com>
2025-10-13 19:31:18 +0530

    Fix formatting in MongoDB connection log message and ensure newline at end of file

--- src/db/index.js
+++ src/db/index.js
@@ -4,8 +4,7 @@ import { DB_NAME } from "../constants.js";
 const connectDB = async () => {
     try {
         const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
-        console.log(`\n Connected to MongoDB!!! : 
-        DB HOST: ${connectionInstance.connection.host}`);
+        console.log(`\n Connected to MongoDB!!!: DB HOST: ${connectionInstance.connection.host}`);
     } 
 
     catch (error) {
@@ -14,3 +13,5 @@ const connectDB = async () => {
     }
 
 }
+
+export default connectDB;
